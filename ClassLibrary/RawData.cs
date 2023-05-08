e59c7cc8a6d0510601a3166f41403cead5b5d560
Bikmish <bikmish@ispras.ru>
2023-05-08 19:04:45 +0300

    Minor refactor: using-operator to avoid use after dispose

--- ClassLibrary/RawData.cs
+++ ClassLibrary/RawData.cs
@@ -25,17 +25,16 @@ namespace ClassLibrary
             for (int i = 0; i < rd.NodeValues.Length; ++i)
                 (rd.NodeCoords[i], rd.NodeValues[i]) = (rd.IsUniform ? rd.EndsCoords[0] + i * delta : rd.EndsCoords[0] + ((i==0 || i== rd.NodeValues.Length-1) ? i : i - new Random().NextDouble()) * delta, rd.Func(i));
         }
-        public RawData(string filename) => Load(filename, this); //out this???
+        public RawData(string filename) => Load(filename, this);
         public static double f1(double x) => x + 5;
         public static double f2(double x) => x * x * x + 1;
         public static double f3(double x) => new Random().NextDouble() * x;
         public bool Save(string filename)
         {
-            (FileStream stream, StreamWriter writer, bool success) = (null, null, true);
             try
             {
-                stream = File.Create(filename);
-                writer = new StreamWriter(stream);
+                using var stream = File.Create(filename);
+                using var writer = new StreamWriter(stream);
                 writer.WriteLine(EndsCoords[0].ToString());
                 writer.WriteLine(EndsCoords[1].ToString());
                 writer.WriteLine(NumNodes.ToString());
@@ -58,23 +57,16 @@ namespace ClassLibrary
             catch (Exception e)
             {
                 Console.WriteLine($"Saving error! {e}\n");
-                success = false;
+                return false;
             }
-            finally
-            {
-                if (writer != null) { writer.Dispose(); }
-                if (stream != null) { stream.Close(); }
-            }
-            return success;
+            return true;
         }
         public static bool Load(string filename, RawData rd)
         {
-            (FileStream stream, StreamReader reader, bool success) = (null, null, true);
             try
             {
-                stream = File.OpenRead(filename);
-                reader = new StreamReader(stream);
-
+                using var stream = File.OpenRead(filename);
+                using var reader = new StreamReader(stream);
                 rd.EndsCoords = new double[2];
                 rd.EndsCoords[0] = Convert.ToDouble(reader.ReadLine());
                 rd.EndsCoords[1] = Convert.ToDouble(reader.ReadLine());
@@ -100,14 +92,9 @@ namespace ClassLibrary
             catch (Exception e)
             {
                 Console.WriteLine($"Loading error! {e}\n");
-                success = false;
+                return false;
             }
-            finally
-            {
-                if (reader != null) { reader.Dispose(); }
-                if (stream != null) { stream.Close(); }
-            }
-            return success;
+            return true;
         }
     }
 }
